c2a12de08e7e26a771177297dcbe8979b2dc06e9
jjoslin0994 <144361203+jjoslin0994@users.noreply.github.com>
2025-04-04 15:11:36 -0400

    Update FrontBumper.ino
    Add GPS and speed interpretation

--- FrontBumper.ino
+++ FrontBumper.ino
@@ -1,12 +1,20 @@
-#define TX_PIN 16
-#define RX_PIN 25
+#define TX_PIN 16 // to 10 on jetson
+#define RX_PIN 25 // to 8 on jetson
 
 #include <Arduino.h>
+#include <Wire.h>
+
+#define TESSEO_I2C_ADDR 0X3A
 
 void setup() {
     Serial.begin(115200);  // USB serial for debugging
+
+    // UART connection with Jetson Nano
     Serial2.begin(115200, SERIAL_8N1, RX_PIN, TX_PIN);
     Serial.println("ESP32 UART TEST: Sending to Jetson");
+
+    // I2C connection with GPS
+    Wire.begin(18,19);
 }
 
 bool transmit = true;
@@ -15,9 +23,14 @@ unsigned long sendInterval = 1000;
 
 bool detectionMode = false;
 
+double speedMPS;
+
 String msg = "Hello Jetson";
 
 void loop() {
+
+  HandleGPS(); // reads GPS and extracts speed in KPH
+
   unsigned long currentTime = millis();
 
   if(currentTime - lastSendTime >= sendInterval)
@@ -122,10 +135,68 @@ void handleDetectedObject(String receivedDetection) {
       Serial.println("Lane Departure!");
     }
 
-    if (distance != "N/A" && distanceDouble > 0 && distanceDouble < 250) {
+    if (isTailingTooClose(distanceDouble, speedMPS)) {
       Serial.println("Tailing Hazard!");
     }
 
+    if(label == "person" && distanceDouble < 500 && speedMPS > 0)
+    {
+      Serial.println("Pedestrian in danger");
+    }
+    
+
+}
+
+String lastScentence = "";
+
+void HandleGPS()
+{
+
+  Wire.requestFrom(TESSEO_I2C_ADDR, 32);
+  String nmeaData = "";
+
+  while(Wire.available())
+  {
+    char c = Wire.read();
+    nmeaData += c;
+  }
+
+  if(nmeaData != lastScentence)
+  {
+    int start = nmeaData.indexOf("$GPRMC");
+    if(start != -1)
+    {
+      int commas = 0, speedIdx= 0;
+      for (int i = start; i < nmeaData.length(); i++)
+      {
+        if(nmeaData[i] == ',')
+        {
+          if(++commas == 7) { speedIdx = i + 1; break; }
+        }
+      }
+
+      int endIdx = nmeaData.indexOf(',', speedIdx);
+      speedMPS = nmeaData.substring(speedIdx, endIdx).toFloat() * 0.5144;
+    }
+  }
+}
+
+bool isTailingTooClose(double tailingDistance, double currentSpeed)
+{
+  // tailingDistance is in mm
+  double safeDistance = currentSpeed * 2.0; // 2-second rule
+
+  if(currentSpeed < 0)
+  {
+    if (tailingDistance < 250)
+    {
+      return true;
+    }
+  }
+  else
+  {
+    return (tailingDistance / 1000) < safeDistance;
+  }
 }
 
 
